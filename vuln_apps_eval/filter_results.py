10ca72067b2df97f74e3a5f2e472d56761ef5665
chrisvobi <christoforidis02@gmail.com>
2026-01-28 20:34:33 +0200

    more dvna results

--- vuln_apps_eval/filter_results.py
+++ vuln_apps_eval/filter_results.py
@@ -5,8 +5,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from utils.llm_filtering import filter_llm_findings, filter_with_existing_responses
 
-project_name = "juice-shop"
-threshold = 0.6
+project_name = "dvna"
+threshold = 0.95
 
 csv_path = rf"C:\Projects\thesis\output\{project_name}_callgraphs1\{project_name}\deduplicated.csv"
 filtered_csv_path = rf"C:\Projects\thesis\output\{project_name}_callgraphs1\{project_name}\filtered{str(int(threshold*100))}_deduplicated.csv"
